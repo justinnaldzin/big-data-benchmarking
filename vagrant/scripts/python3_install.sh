f182f8acfdb5a7c0369a99f7bd4952e502fab431
justinnaldzin <justin@Justins-MacBook-Pro.local>
2017-01-26 23:08:56 -0500

    python script update

--- vagrant/scripts/python3_install.sh
+++ vagrant/scripts/python3_install.sh
@@ -8,8 +8,6 @@ yum -y install https://centos7.iuscommunity.org/ius-release.rpm   # Inline with 
 yum -y install python35u-3.5.2  # Python 3.5.2
 yum -y install python35u-pip  # pip
 yum -y install python35u-devel  # libraries and header files
-yum -y install python-tools  # 2to3 tool
-yum -y install cyrus-sasl-devel
 sudo ln -s /usr/bin/python3.5 /usr/bin/python3  # create a symbolic link "python3" to point to "python3.5"
 sudo ln -s /usr/bin/pip3.5 /usr/bin/pip3  # create a symbolic link "pip3" to point to "pip3.5"
 
@@ -21,16 +19,27 @@ echo "/usr/lib/oracle/12.1/client64/lib/" > /etc/ld.so.conf.d/oracle.conf  # add
 ldconfig  # enable the shared library path system wide
 
 # Add oracle library paths to bash profile for 'root' user
-echo 'export ORACLE_VERSION="12.1"' >> /etc/profile.d/oracle.sh
-echo 'export ORACLE_HOME="/usr/lib/oracle/$ORACLE_VERSION/client64"' >> /etc/profile.d/oracle.sh
-echo 'export PATH=$PATH:"$ORACLE_HOME/bin"' >> /etc/profile.d/oracle.sh
-echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$ORACLE_HOME/lib"' >> /etc/profile.d/oracle.sh
-source /etc/profile.d/oracle.sh
+echo 'export ORACLE_VERSION="12.1"' >> $HOME/.bashrc
+echo 'export ORACLE_HOME="/usr/lib/oracle/$ORACLE_VERSION/client64"' >> $HOME/.bashrc
+echo 'export PATH=$PATH:"$ORACLE_HOME/bin"' >> $HOME/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$ORACLE_HOME/lib"' >> $HOME/.bashrc
+. $HOME/.bashrc
 mkdir -p $ORACLE_HOME/network/admin/
 cp /benchmarking/vagrant/installs/tnsnames.ora $ORACLE_HOME/network/admin/
 
+# Add oracle library paths to bash profile for 'vagrant' user
+echo 'export ORACLE_VERSION="12.1"' >> /home/vagrant/.bashrc
+echo 'export ORACLE_HOME="/usr/lib/oracle/$ORACLE_VERSION/client64"' >> /home/vagrant/.bashrc
+echo 'export PATH=$PATH:"$ORACLE_HOME/bin"' >> /home/vagrant/.bashrc
+echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$ORACLE_HOME/lib"' >> /home/vagrant/.bashrc
+
 # Install additional Python libraries
 pip3 install --upgrade pip setuptools wheel virtualenv
-pip3 install -r /benchmarking/vagrant/installs/pip_dependencies.txt
-/usr/bin/2to3 -w /usr/lib/python3.5/site-packages/sqlalchemy_hana/  # transform the package from Python 2.x to 3.x
-/usr/bin/2to3 -w /usr/lib/python3.5/site-packages/pyhive/
+pip3 install requests pandas bokeh inflection
+pip3 install cx_Oracle  # Python interface to Oracle
+pip3 install pymssql  # DB-API interface to Microsoft SQL Server for Python.
+pip3 install pyhdb  # SAP HANA Database Client for Python
+pip3 install pyhive  # Python interface to Hive
+pip3 install csvkit  # Suite of utilities for converting to and working with CSV.
+#pip install ??????  # Spark SQL
+
