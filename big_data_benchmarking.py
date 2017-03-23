a1b2f7e44a69fb72a9cc497a47eb67bd1ef73fc7
justinnaldzin <NaldzinJ@uphs.pennhealth.prv>
2017-03-23 10:15:26 -0400

    add 'table-like' argument and functionality

--- big_data_benchmarking.py
+++ big_data_benchmarking.py
@@ -71,13 +71,11 @@ def main(args):
             engine = create_engine(attributes['connection_string'])
             if not args['create_tables']:  # Use existing tables in database
                 logging.info('############  Querying ' + database + ' for all table names  ############')
-                sql = attributes['table_name_query']
-                ########################################################################################################
-                #  DEBUG
-                #sql = sql + " WHERE TABLE_NAME LIKE 'On_Time_Performance_2016%'"
+                sql = attributes['table_name_query'].format(table_like=args['table_like'])
                 logging.info(sql)
                 tables_dataframe = pandas.read_sql(sql, engine)
                 tables_dataframe.columns = tables_dataframe.columns.str.lower()  # SQLAlchemy column case sensitivity is inconsistent between SQL dialects
+                tables_dataframe.sort_values('table_name', axis=1, inplace=True)
                 logging.info('Found the following table names:')
                 [logging.info(table_name) for table_name in tables_dataframe['table_name']]
             else:  # Use data files on local file system to create tables and insert into database
@@ -89,6 +87,7 @@ def main(args):
                     sys.exit(1)
                 tables_dataframe = pandas.DataFrame({'table_name': [(os.path.splitext(os.path.basename(filename))[0])
                                                                     for filename in data_filepath_list]})
+                tables_dataframe.sort_values('table_name', axis=1, inplace=True)
                 logging.info('Found the following files in path:  ' + str(data_path))
                 [logging.info(filename) for filename in data_filepath_list]
 
@@ -152,21 +151,26 @@ if __name__ == '__main__':
     parser.add_argument('database_list', nargs='*', default=[],
                         help="Specify the list of Databases to benchmark.  These names must match the names "
                              "pre-configured in the 'config.json' file.")
+    parser.add_argument('-t', '--table-like', dest='table_like', default='%',
+                        type=str, help="Specify the name of the tables to benchmark.  This uses the SQL 'LIKE' operator"
+                                       " to search a specified pattern so use the '%%' sign to define wildcards.  "
+                                       "Default is '%%' which will find all existing tables in the database.")
     parser.add_argument('-r', '--rows', dest='rows', default=10000, type=int,
                         help="The maximum number of rows to return from each query execution.  Default is 10000")
     parser.add_argument('-i', '--iterations', dest='iterations', default=1, type=int,
                         help="The number of benchmark iterations to perform on the database.  Default is 1")
     parser.add_argument('-u', '--users', dest='concurrent_users', default=1, type=int,
                         help="The number of concurrent users to connect to the database.  Default is 1")
-    parser.add_argument('-p', '--path', dest='data_path', default=os.path.join(script_dir + os.path.sep + 'data'), type=str,
-                        help="Full directory path to where the data files are stored.  These will be used to create "
-                             "the tables and insert into database.  Default path is:  /data")
+    parser.add_argument('-p', '--path', dest='data_path', default=os.path.join(script_dir + os.path.sep + 'data'),
+                        type=str, help="Full directory path to where the data files are stored.  These will be used to "
+                                       "create the tables and insert into database.  Default path is:  /data")
     parser.add_argument('-c', '--create-tables', dest='create_tables', action='store_true',
                         help="Create tables and insert into database the data files that exist within in the folder "
                              "'--path' argument.  Not specifying this option will run benchmarks on all existing "
-                             "tables in the database")
+                             "tables in the database.")
     parser.add_argument('-d', '--drop-tables', dest='drop_tables', action='store_true',
                         help="The '--create-tables' argument must be specified.  Only those tables created will be dropped.")
+
     args = vars(parser.parse_args())
     if args['drop_tables'] and not args['create_tables']:
         parser.error("[-d], [--drop-tables] requires [-c], [--create-tables].  Use [-h] for more help.")
