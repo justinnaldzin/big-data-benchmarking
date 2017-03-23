01a9e51ab3b57b9aac793baba7ba1443a1d7dede
justinnaldzin <NaldzinJ@uphs.pennhealth.prv>
2017-03-23 10:19:48 -0400

    add query  timeout and retry functionality

--- benchmark.py
+++ benchmark.py
@@ -2,14 +2,13 @@
 
 
 import os
-import sys
 import time
 import logging
-import threading
 import pandas
 from random import randint
 from retrying import retry
 from sqlalchemy import create_engine
+from pebble import concurrent
 
 
 class Timer:
@@ -22,45 +21,35 @@ class Timer:
         self.interval = self.end - self.start
 
 
-class Timeout(Exception):
+class TimeoutException(Exception):
     pass
 
 
-def timelimit(timeout):
-    def internal(function):
-        def internal2(*args, **kw):
-            class Calculator(threading.Thread):
-                def __init__(self):
-                    threading.Thread.__init__(self)
-                    self.result = None
-                    self.error = None
-
-                def run(self):
-                    try:
-                        self.result = function(*args, **kw)
-                    except:
-                        self.error = sys.exc_info()[0]
-            c = Calculator()
-            c.start()
-            c.join(timeout)
-            if c.isAlive():
-                raise Timeout
-            if c.error:
-                raise c.error
-            return c.result
-        return internal2
-    return internal
-
-
-@retry(stop_max_attempt_number=6)  # stop after 6 attempts
-@timelimit(3600)  # timeout after 600 seconds (10 minutes)
-def query(engine, query_template, table_name, datatypes_dataframe, rows):
-    query_builder_dict = query_builder(table_name, datatypes_dataframe, rows)
-    sql = query_template.format(**query_builder_dict)
-    logging.debug(sql)
+@concurrent.process(timeout=1800)  # timeout after 3600 seconds (60 minutes)
+def query(sql, engine):
+    """
+    Uses the pebble.concurrent.process decorator, this function will timeout after the specified time frame has passed.
+    Executes the sql query using the engine, returning the time in seconds it takes the query to return all results as
+    well as returns the number of rows from the query results.
+    """
     with Timer() as t:
         dataframe = pandas.read_sql(sql, engine)
         rows = len(dataframe.index)
+    return rows, t
+
+
+@retry(stop_max_attempt_number=6)  # stop after 6 attempts
+def build_query(engine, query_template, table_name, datatypes_dataframe, rows):
+    """
+    Using the retry decorator, will retry up to the specified number of attempts.  Calls the query_builder function,
+    formats the sql query, then calls the query function returning the formatted sql query, the number of rows from the
+    query result, and the formatted query execution time.
+    """
+    query_builder_dict = query_builder(table_name, datatypes_dataframe, rows)
+    sql = query_template.format(**query_builder_dict)
+    logging.debug("Executing query:  " + sql)
+    engine.dispose()  # dispose of the connection pool and create a new connection pool immediately
+    rows, t = query(sql, engine).result()  # blocks until results are ready
     return sql, rows, float(t.interval)
 
 
@@ -89,8 +78,8 @@ def query_builder(table_name, datatypes_dataframe, rows):
 def database(queries_dataframe, attributes, tables_dataframe, csv_filepath, args):
     """
     Benchmark the database based on the number of iterations specified in the arg['iterations'] parameter.  Execute
-    every query from the 'queries_dataframe' parameter against every table in the 'tables_dataframe' parameter.  Write the
-    execution time to the 'csv_filepath' parameter.
+    every query from the 'queries_dataframe' parameter against every table in the 'tables_dataframe' parameter.  Write
+    the execution time to the 'csv_filepath' parameter.
     """
     engine = create_engine(attributes['connection_string'])
     for i in range(args['iterations']):
@@ -103,11 +92,17 @@ def database(queries_dataframe, attributes, tables_dataframe, csv_filepath, args
             datatypes_dataframe.columns = map(str.lower, datatypes_dataframe.columns)  # SQLAlchemy column case sensitivity is inconsistent between SQL dialects
             for query_index, query_row in queries_dataframe.iterrows():
                 try:
-                    (query_row['query_executed'], query_row['rows'], query_row['time']) = query(engine, query_row['query_template'], table_row['table_name'], datatypes_dataframe, args['rows'])
+                    (query_row['query_executed'], query_row['rows'], query_row['time']) = build_query(engine, query_row['query_template'], table_row['table_name'], datatypes_dataframe, args['rows'])
                     logging.info("Query " + str(query_row['query_id']) + str(':  {:f} sec'.format(query_row['time'])))
-                except Timeout:
+                except TimeoutException as error:
+                    print("Function took longer than %d seconds" % error.args[1])
                     (query_row['query_executed'], query_row['rows'], query_row['time']) = ('Timeout!', 0, 600)
                     logging.warning("Timeout!  " + "Query " + str(query_row['query_id']) + str(':  {:f} sec'.format(query_row['time'])))
+                except Exception as error:
+                    print("Function raised %s" % error)
+                    (query_row['query_executed'], query_row['rows'], query_row['time']) = ('Timeout!', 0, 600)
+                    logging.warning("Timeout!  " + "Query " + str(query_row['query_id']) + str(
+                        ':  {:f} sec'.format(query_row['time'])))
                 query_row['concurrency_factor'] = int(args['concurrent_users'])
                 query_row = pandas.concat([query_row, table_row])
                 benchmark_dataframe = benchmark_dataframe.append(query_row, ignore_index=True)
